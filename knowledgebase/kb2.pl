8371ecf21514b7fcf3fd40f214b0570d059df387
TechnologyHell <nitishsinghslg@gmail.com>
2024-07-04 10:37:12 +0530

    kb

---
+++ knowledgebase/kb2.pl
@@ -0,0 +1,6 @@
+sing_a_song(ananya).
+listens_to_music(rohit).
+listens_to_music(ananya).
+happy(ananya) :- sing_a_song(ananya).
+happy(rohit) :- listens_to_music(rohit).
+plays_guitar(rohit) :- listens_to_music(rohit).
